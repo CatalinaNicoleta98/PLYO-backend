03c3065e7029a1f0bb6133fc22d3b7c95865d298
Catalina Vrinceanu <cnv98@yahoo.com>
2026-02-27 12:56:36 +0100

    added db connection and initial api route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, {Application, Request, Response} from 'express';
 import dotenvFlow from 'dotenv-flow';
-
+import routes from './routes';
+import {testConnection} from '../repository/db';
 import test from 'node:test';
 
 
@@ -10,9 +11,11 @@ dotenvFlow.config();
 //create express application
 const app: Application = express();
 
+app.use('/api', routes);
+
 export function startServer(){
 
-   
+   testConnection();
 const PORT: number = parseInt(process.env.PORT as string) || 4000;
     app.listen(PORT, function(){
         console.log("Server is up and running on port:" + PORT);
